2c537be173fe1dab9db864bb94b1eece60cff1da
juansesu3 <juanseu3@hotmail.com>
2023-05-15 14:15:26 +0200

    Navbar design

--- components/Center.js
+++ components/Center.js
@@ -2,7 +2,8 @@ import { styled } from "styled-components";
 
 const StyleDiv = styled.div`
   max-width: 800px;
-  margin: 0 20px;
+  margin: 0 auto;
+  padding: 0 20px;
 `;
 const Center = ({ children }) => {
   return <StyleDiv>{children}</StyleDiv>;
--- components/Header.js
+++ components/Header.js
@@ -10,19 +10,37 @@ const Logo = styled(Link)`
   text-decoration: none;
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+`;
+
+const StyleNav = styled.nav`
+    display:flex;
+    gap:15px;
+
+`;
+
+const NavLink = styled(Link)`
+  color: #aaa;
+  text-decoration: none;
+`;
+
 const Header = () => {
   return (
     <StyleHeader>
       <Center>
-        
-        <Logo href={"/"}>Ecommerce</Logo>
-        <nav>
-          <Link href={"/"}>Home</Link>
-          <Link href={"/products"}>All products</Link>
-          <Link href={"/categories"}>Categories</Link>
-          <Link href={"/account"}>Account</Link>
-          <Link href={"/cart"}>Cart (0)</Link>
-        </nav>
+        <Wrapper>
+          <Logo href={"/"}>Ecommerce</Logo>
+          <StyleNav>
+            <NavLink href={"/"}>Home</NavLink>
+            <NavLink href={"/products"}>All products</NavLink>
+            <NavLink href={"/categories"}>Categories</NavLink>
+            <NavLink href={"/account"}>Account</NavLink>
+            <NavLink href={"/cart"}>Cart (0)</NavLink>
+          </StyleNav>
+        </Wrapper>
       </Center>
     </StyleHeader>
   );
