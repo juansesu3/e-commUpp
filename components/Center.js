e7419ac8ac02d52d510af61ed49d113bf0cbacd6
juansesu3 <juanseu3@hotmail.com>
2023-05-15 14:01:30 +0200

    global initial  configuration

---
+++ components/Center.js
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+const StyleDiv = styled.div`
+  max-width: 800px;
+  margin: 0 20px;
+`;
+const Center = ({ children }) => {
+  return <StyleDiv>{children}</StyleDiv>;
+};
+
+export default Center;
