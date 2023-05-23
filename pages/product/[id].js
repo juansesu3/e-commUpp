579408a25d16e2d87762fef3c20f4df68be5f2ce
juansesu3 <juanseu3@hotmail.com>
2023-05-23 13:35:11 +0200

    finished with responsive design

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -13,9 +13,13 @@ import { styled } from "styled-components";
 
 const ColWrapper = styled.div`
   display: grid;
-  grid-template-columns: 0.8fr 1.2fr;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 0.8fr 1.2fr;
+    
+  }
   gap: 40px;
-  margin-top: 40px;
+  margin: 40px 0 ;
 `;
 
 const PriceRow = styled.div`
