f20fe5b75f2c7ad212f83d0ead14b77996e320ee
juansesu3 <juanseu3@hotmail.com>
2023-05-23 10:35:54 +0200

    finished with single page product and I'm going to start front responsive design

---
+++ components/ProductImages.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+
+const Image = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+const BigImage = styled.img`
+  max-width: 100%;
+  max-height: 200px;
+`;
+
+const ImageButtons = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-grow: 0;
+  margin-top: 10px;
+`;
+
+const ImageButton = styled.div`
+border: 2px solid #ccc;
+  ${(props) =>
+    props.active
+      ? `
+  border-color: #ccc;
+  `
+      : `
+  border-color: transparent;
+  opacity:.7;
+  `}
+  
+  height: 30px;
+  padding: 0px;
+  cursor: pointer;
+  border-radius: 5px;
+`;
+const BigImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const ProductImages = ({ images }) => {
+  const [activeImage, setActiveImage] = useState(images?.[0]);
+  return (
+    <>
+      <BigImageWrapper>
+        <BigImage src={activeImage} alt="products-images" />
+      </BigImageWrapper>
+      <ImageButtons>
+        {images.map((image) => (
+          <ImageButton
+            key={image}
+            active={image === activeImage}
+            onClick={() => setActiveImage(image)}
+          >
+            <Image src={image} alt="all.images-products" />
+          </ImageButton>
+        ))}
+      </ImageButtons>
+    </>
+  );
+};
+export default ProductImages;
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,15 +1,61 @@
+import Button from "@/components/Button";
+import { CartContext } from "@/components/CartContext";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
+import ProductImages from "@/components/ProductImages";
 import Title from "@/components/Title";
+import WhiteBox from "@/components/WhiteBox";
+import CartIcon from "@/components/icons/CartIcon";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import { useContext } from "react";
+import { styled } from "styled-components";
+
+const ColWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 0.8fr 1.2fr;
+  gap: 40px;
+  margin-top: 40px;
+`;
+
+const PriceRow = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;
+const Price = styled.span`
+  font-size: 1.4rem;
+`;
 
 const ProductPage = ({ product }) => {
+    const {addProduct} = useContext(CartContext);
   return (
     <>
       <Header />
       <Center>
-        <Title>{product.title}</Title>
+        <ColWrapper>
+          <WhiteBox>
+            <ProductImages images={product.images} />
+          </WhiteBox>
+          <div>
+            <Title>{product.title}</Title>
+            <p>{product.description}</p>
+            <PriceRow>
+              <div>
+                <Price>${product.price}</Price>
+              </div>
+
+              <div>
+                <Button 
+                primary={1}
+                onClick={()=>addProduct(product._id)}
+                >
+                  <CartIcon /> Add to cart
+                </Button>
+              </div>
+            </PriceRow>
+          </div>
+        </ColWrapper>
       </Center>
     </>
   );
