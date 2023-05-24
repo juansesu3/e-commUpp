69816ef6a9f5c545edbeb172aef47e6b6faf995a
juansesu3 <juanseu3@hotmail.com>
2023-05-24 09:56:55 +0200

    working on filters

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -4,13 +4,81 @@ import ProductsGrid from "@/components/ProductsGrid";
 import Title from "@/components/Title";
 import { Category } from "@/models/Category";
 import { Product } from "@/models/Product";
+import { useState } from "react";
+import { styled } from "styled-components";
+
+const CategoryHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 1.5em;
+  }
+`;
+
+const FiltersWrapper = styled.div`
+  display: flex;
+  gap: 15px;
+`;
+
+const Filter = styled.div`
+  background-color: #ddd;
+  padding: 5px 10px;
+  border-radius: 5px;
+  display: flex;
+  gap: 5px;
+  color: #444;
+  select {
+    background-color: transparent;
+    border: 0;
+    font-size: inherit;
+    color: #444;
+  }
+`;
 
 const CategoryPage = ({ category, products }) => {
+  const [filtersValues, setFiltersValues] = useState(
+    category.properties.map((p) => ({ name: p.name, value: "all" }))
+  );
+
+  const handlerFilterChange = (filterName, filterValue) => {
+    setFiltersValues((prev) => {
+      return prev.map((p) => ({
+        name: p.name,
+        value: p.name === filterName ? filterValue : p.value,
+      }));
+    });
+  };
+
   return (
     <>
       <Header />
       <Center>
-        <Title>{category.name}</Title>
+        <CategoryHeader>
+          <h1>{category.name}</h1>
+          <FiltersWrapper>
+            {category.properties.map((prop) => (
+              <Filter key={prop.name}>
+                <span>{prop.name}: </span>
+                <select
+                  onChange={(ev) =>
+                    handlerFilterChange(prop.name, ev.target.value)
+                  }
+                  value={filtersValues.find((f) => f.name === prop.name).value}
+                >
+                  <option value="all">All</option>
+                  {prop.values.map((val) => (
+                    <option key={val} value={val}>
+                      {val}
+                    </option>
+                  ))}
+                </select>
+              </Filter>
+            ))}
+          </FiltersWrapper>
+        </CategoryHeader>
+
         <ProductsGrid products={products} />
       </Center>
     </>
