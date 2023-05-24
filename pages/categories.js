cda114e6e793bd61d8cf29a8d9f8c5a8f7a6697c
juansesu3 <juanseu3@hotmail.com>
2023-05-24 07:41:31 +0200

    working on category page

--- pages/categories.js
+++ pages/categories.js
@@ -1,8 +1,51 @@
 import Center from "@/components/Center";
 import Header from "@/components/Header";
+import ProductBox from "@/components/ProductBox";
 import Title from "@/components/Title";
 import { Category } from "@/models/Category";
 import { Product } from "@/models/Product";
+import Link from "next/link";
+import { styled } from "styled-components";
+
+const CategoryGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`;
+
+const CategoryTitle = styled.div`
+  display: flex;
+  margin-top: 40px;
+  margin-bottom: 0;
+  align-items: center;
+  gap: 20px;
+  h2 {
+    margin-bottom: 10px;
+    margin-top: 10px;
+  }
+  a {
+    color: #555;
+    display: inline-block;
+  }
+`;
+
+const CategoryWrapper = styled.div`
+  margin-bottom: 40px;
+`;
+
+const ShowAllSquare = styled(Link)`
+  display: flex;
+  background-color: #ddd;
+  height: 160px;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  color: #555;
+  text-decoration: none;
+`;
 
 const CategoriesPage = ({ mainCategories, categoriesProducts }) => {
   return (
@@ -11,14 +54,23 @@ const CategoriesPage = ({ mainCategories, categoriesProducts }) => {
       <Center>
         <Title>
           {mainCategories?.map((cat) => (
-            <div key={cat._id}>
-              <h2>{cat.name}</h2>
-              <div>
+            <CategoryWrapper key={cat._id}>
+              <CategoryTitle>
+                <h2>{cat.name}</h2>
+                <div>
+                  <Link href={"/category/" + cat._id}>Show all</Link>
+                </div>
+              </CategoryTitle>
+
+              <CategoryGrid>
                 {categoriesProducts[cat._id].map((p) => (
-                  <div key={p._id}>{p.title}</div>
+                  <ProductBox key={p._id} {...p} />
                 ))}
-              </div>
-            </div>
+                <ShowAllSquare href={"category" + cat._id}>
+                  Show all &rarr;
+                </ShowAllSquare>
+              </CategoryGrid>
+            </CategoryWrapper>
           ))}
         </Title>
       </Center>
