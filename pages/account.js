7c2971e093fffc8750375e0b4a8ec54c9a01dd7f
juansesu3 <juanseu3@hotmail.com>
2023-05-25 21:49:59 +0200

    working on account page

--- pages/account.js
+++ pages/account.js
@@ -1,37 +1,126 @@
 import Button from "@/components/Button";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
+import Input from "@/components/Input";
 import Title from "@/components/Title";
+import WhiteBox from "@/components/WhiteBox";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { RevealWrapper } from "next-reveal";
+import { useState } from "react";
+import { styled } from "styled-components";
+
+const CityHolder = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+const ColsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1.2fr 0.8fr;
+  gap: 40px;
+  margin: 40px 0;
+`;
 
 const AccountPage = () => {
-  const {data:session} = useSession();
+  const { data: session } = useSession();
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [streetAddress, setStreeAddres] = useState("");
+  const [country, setCountry] = useState("");
 
-  const Logout = async  () => {
+  const Logout = async () => {
     await signOut({
       callbackUrl: process.env.NEXT_PUBLIC_URL,
     });
   };
 
-  const Login = async ()=>{
-    await signIn('google')
-  }
+  const Login = async () => {
+    await signIn("google");
+  };
 
   return (
     <>
       <Header />
       <Center>
-        
-        <Title>Account</Title>
-        {session && (
-          <Button primary={1} onClick={Logout}>
-            Logout
-          </Button>
-        )}
-        {!session && (
-            <Button primary={1} onClick={Login}>Login</Button>
-        )}
+        <ColsWrapper>
+          <div>
+            <RevealWrapper delay={0}>
+              <WhiteBox>
+                <h2>WishList</h2>
+              </WhiteBox>
+            </RevealWrapper>
+          </div>
+          <div>
+            <RevealWrapper delay={100}>
+              <WhiteBox>
+                <h2>Accoubt Details</h2>
+
+                <Input
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  name="name"
+                  onChange={(ev) => setName(ev.target.value)}
+                />
+                <Input
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  name="email"
+                  onChange={(ev) => setEmail(ev.target.value)}
+                />
+                <CityHolder>
+                  <Input
+                    type="text"
+                    placeholder="City"
+                    value={city}
+                    name="city"
+                    onChange={(ev) => setCity(ev.target.value)}
+                  />
+                  <Input
+                    type="text"
+                    placeholder="Postal Code"
+                    value={postalCode}
+                    name="postalCode"
+                    onChange={(ev) => setPostalCode(ev.target.value)}
+                  />
+                </CityHolder>
+                <Input
+                  type="text"
+                  placeholder="Street Addres"
+                  value={streetAddress}
+                  name="streeAdress"
+                  onChange={(ev) => setStreeAddres(ev.target.value)}
+                />
+                <Input
+                  type="text"
+                  placeholder="Country"
+                  value={country}
+                  name="country"
+                  onChange={(ev) => setCountry(ev.target.value)}
+                />
+
+                <Button black={1} block={1} onClick={() => {}}>
+                  Save
+                </Button>
+                <hr />
+                {session && (
+                  <Button primary={1} onClick={Logout}>
+                    Logout
+                  </Button>
+                )}
+                {!session && (
+                  <Button primary={1} onClick={Login}>
+                    Login
+                  </Button>
+                )}
+              </WhiteBox>
+            </RevealWrapper>
+          </div>
+        </ColsWrapper>
       </Center>
     </>
   );
